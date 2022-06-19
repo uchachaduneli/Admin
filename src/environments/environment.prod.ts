3f3488fea8a6f92c49999a97294ebce925549e0f
Ucha Chaduneli <uchachaduneli@gmail.com>
2022-06-20 00:33:10 +0400

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://2.240.0.138/DeliveryWs'
-  apiUrl: 'http://212.72.143.213:8090/DeliveryWs'
+  apiUrl: 'https://api.exline.ge/DeliveryWs'
+  // apiUrl: 'http://212.72.143.213:8090/DeliveryWs'
   // apiUrl: '/DeliveryWs'
 };
